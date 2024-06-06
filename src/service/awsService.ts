414eb64592378e79cdb78508a8390bc090ea8859
Ian Hwang <meen813@hotmail.com>
2024-06-05 17:34:01 -0700

    feat: add envrionmental variables

--- src/service/awsService.ts
+++ src/service/awsService.ts
@@ -1,8 +1,14 @@
 import AWS from 'aws-sdk';
 import { Buffer } from 'buffer';
 
-const rekognition = new AWS.Rekognition({ region: 'us-west-1' });
-const s3 = new AWS.S3({ region: 'us-west-1' });
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const rekognition = new AWS.Rekognition();
+const s3 = new AWS.S3();
 const bucketName = 'my-image-rekognition-bucket';
 
 interface RekognitionLabel {
